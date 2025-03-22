bcb42f9fa2de7fd67cbeb6da3d05e6f742619981
Vinny Kylex <vinnykylex@gmail.com>
2025-03-22 20:47:44 +0300

    march 22 c

--- config/queue.js
+++ config/queue.js
@@ -4,29 +4,9 @@ import Redis from 'ioredis';
 // Use Upstash Redis connection string
 const redisUrl = process.env.REDIS_URL;
 
-// Create Redis clients for Bull
-const redisClient = new Redis(redisUrl, {
-  enableReadyCheck: false, // Fixes Bull queue issue
-  maxRetriesPerRequest: null
-});
-
-const redisSubscriber = new Redis(redisUrl, {
-  enableReadyCheck: false,
-  maxRetriesPerRequest: null
-});
-
-// Create payment queue
+// Create payment queue with correct configuration
 const paymentQueue = new Queue('payment-processing', {
-  createClient: (type) => {
-    switch (type) {
-      case 'client':
-        return redisClient;
-      case 'subscriber':
-        return redisSubscriber;
-      default:
-        return new Redis(redisUrl);
-    }
-  },
+  redis: redisUrl,
   defaultJobOptions: {
     attempts: 3,
     backoff: {
